42124b719b366cb0fcfe635c5dd4395f12057a0a
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2024-12-11 19:42:52 -0300

    feat: adiciona endpoint DeleteCategoryEndpoint

---
+++ src/Dima.Api/Endpoints/Categories/DeleteCategoryEndpoint.cs
@@ -0,0 +1,32 @@
+using Dima.Api.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Endpoints.Categories;
+
+public class DeleteCategoryEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapPost("/{id:long}", HandleAsync)
+            .WithName("Categories: Delete")
+            .WithSummary("Remove uma categoria")
+            .WithDescription("Remove uma categoria")
+            .WithOrder(3)
+            .Produces<Response<Category?>>();
+    
+    private static async Task<IResult> HandleAsync(ICategoryHandler handler, long id)
+    {
+        var request = new DeleteCategoryRequest
+        {
+            Id = id,
+            UserId = "teste@balta.io"
+        };
+        var result = await handler.DeleteAsync(request);
+        
+        return result.IsSuccess 
+            ? TypedResults.NoContent()
+            : Results.BadRequest(result);
+    }
+}
