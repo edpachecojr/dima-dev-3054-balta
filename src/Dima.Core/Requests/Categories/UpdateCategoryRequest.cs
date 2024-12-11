b7be5fe42a38e2f116738c2eb5beaf8194984f30
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2024-12-11 12:49:22 -0300

    fix: adiciona Id em UpdateCategoryRequest

--- src/Dima.Core/Requests/Categories/UpdateCategoryRequest.cs
+++ src/Dima.Core/Requests/Categories/UpdateCategoryRequest.cs
@@ -4,10 +4,13 @@ namespace Dima.Core.Requests.Categories;
 
 public class UpdateCategoryRequest : Request
 {
+    [Required(ErrorMessage = "Id é obrigatório")]
+    public long Id { get; set; }
     [Required(ErrorMessage = "Título inválido")]
     [MaxLength(80, ErrorMessage = "O título deve ter no máximo 80 caracteres")]
     public string Title { get; set; } = string.Empty;
     
     [Required(ErrorMessage = "Descrição inválida")]
     public string Description { get; set; } = string.Empty;
+
 }
