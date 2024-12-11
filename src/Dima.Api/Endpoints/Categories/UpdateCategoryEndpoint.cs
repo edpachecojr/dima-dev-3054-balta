3b62aa94fab01ac9ab20660f444280f526f64ccb
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2024-12-11 19:40:46 -0300

    refactor: adiciona tipagem parametro id em UpdateCategoryEndpoint

--- src/Dima.Api/Endpoints/Categories/UpdateCategoryEndpoint.cs
+++ src/Dima.Api/Endpoints/Categories/UpdateCategoryEndpoint.cs
@@ -9,15 +9,16 @@ namespace Dima.Api.Endpoints.Categories;
 public class UpdateCategoryEndpoint : IEndpoint
 {
     public static void Map(IEndpointRouteBuilder app)
-        => app.MapPut("/", HandleAsync)
+        => app.MapPut("/{id:long}", HandleAsync)
             .WithName("Categories: Update")
             .WithSummary("Altera uma categoria")
             .WithDescription("Altera uma categoria")
             .WithOrder(2)
             .Produces<Response<Category?>>();
     
-    private static async Task<IResult> HandleAsync(ICategoryHandler handler, UpdateCategoryRequest request)
+    private static async Task<IResult> HandleAsync(ICategoryHandler handler, UpdateCategoryRequest request, long id)
     {
+        request.Id = id;
         var result = await handler.UpdateAsync(request);
         
         return result.IsSuccess 
