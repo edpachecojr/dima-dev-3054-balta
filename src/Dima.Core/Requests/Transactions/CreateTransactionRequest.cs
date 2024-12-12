8364521ff8e39e66deea7abbdd745d0caf6f935e
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2024-12-12 09:54:50 -0300

    feat: adiciona CreateTransactionRequest

---
+++ src/Dima.Core/Requests/Transactions/CreateTransactionRequest.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using Dima.Core.Enums;
+
+namespace Dima.Core.Requests.Transactions;
+
+public class CreateTransactionRequest : Request
+{
+    [Required(ErrorMessage = "Título inválido")]
+    public string Title { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Tipo inválido")]
+    public ETransactionType Type { get; set; }
+    
+    [Required(ErrorMessage = "Amount inválido")]
+    public decimal Amount { get; set; }
+    
+    [Required(ErrorMessage = "Categoria inválida")]
+    public long CategoryId { get; set; }
+    
+    [Required(ErrorMessage = "Data inválido")]
+    public DateTimeOffset? PaidOrReceivedAt { get; set; }
+}
