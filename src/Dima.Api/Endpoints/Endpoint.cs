41b0ab4db10b77b42075b9b085f94b4819746b3f
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2024-12-13 13:25:18 -0300

    refactor: cria extensoes para startup da aplicacao e organiza codigo em Program

--- src/Dima.Api/Endpoints/Endpoint.cs
+++ src/Dima.Api/Endpoints/Endpoint.cs
@@ -1,6 +1,8 @@
 using Dima.Api.Common.Api;
 using Dima.Api.Endpoints.Categories;
+using Dima.Api.Endpoints.Identity;
 using Dima.Api.Endpoints.Transactions;
+using Dima.Api.Models;
 
 namespace Dima.Api.Endpoints;
 
@@ -9,6 +11,9 @@ public static class Endpoint
     public static void MapEndpoints(this WebApplication app)
     {
         var endpoints = app.MapGroup("");
+        endpoints.MapGroup("/")
+            .WithTags("Health Check")
+            .MapGet("/", () => new { message = "OK" });
 
         endpoints.MapGroup("v1/categories")
             .WithTags("Categories")
@@ -18,7 +23,7 @@ public static class Endpoint
             .MapEndpoint<DeleteCategoryEndpoint>()
             .MapEndpoint<GetCategoryByIdEndpoint>()
             .MapEndpoint<GetAllCategoriesEndpoint>();
-        
+
         endpoints.MapGroup("v1/transactions")
             .WithTags("Transactions")
             .RequireAuthorization()
@@ -27,6 +32,15 @@ public static class Endpoint
             .MapEndpoint<DeleteTransactionEndpoint>()
             .MapEndpoint<GetTransactionByIdEndpoint>()
             .MapEndpoint<GetTransactionsByPeriodEndpoint>();
+        
+        endpoints.MapGroup("/v1/identity")
+            .WithTags("Identity")
+            .MapIdentityApi<User>();
+        
+        endpoints.MapGroup("/v1/identity")
+            .WithTags("Identity")
+            .MapEndpoint<LogoutEndpoint>()
+            .MapEndpoint<GetRolesEndpoint>();
     }
 
     private static IEndpointRouteBuilder MapEndpoint<TEndpoint>(this IEndpointRouteBuilder app)
--- src/Dima.Api/Program.cs
+++ src/Dima.Api/Program.cs
@@ -1,76 +1,20 @@
-using System.Security.Claims;
-using Dima.Api.Data;
+using Dima.Api.Common.Api;
 using Dima.Api.Endpoints;
-using Dima.Api.Handlers;
-using Dima.Api.Models;
-using Dima.Core.Handlers;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<AppDbContext>(x => { x.UseSqlServer(connectionString); });
-
-builder.Services.AddIdentityApiEndpoints<User>()
-    .AddRoles<IdentityRole<long>>()
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddApiEndpoints();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(x => { x.CustomSchemaIds(n => n.FullName); });
-
-
-builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
-builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
-
-builder.Services.AddAuthentication();
-builder.Services.AddAuthorization();
+builder.AddConfiguration();
+builder.AddSecurity();
+builder.AddDataContexts();
+builder.AddDocumentation();
+builder.AddServices();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+    app.ConfigureDevEnvironment();
 
-
-app.MapGet("/", () => new { message = "Ok"});
+app.UseSecurity();
 app.MapEndpoints();
-
-app.MapGroup("/v1/identity")
-    .WithTags("Identity")
-    .MapIdentityApi<User>();
-
-app.MapGroup("v1/identity")
-    .WithTags("Identity")
-    .MapPost("/logout", (SignInManager<User> signInManager) =>
-    {
-        signInManager.SignOutAsync();
-        return Results.Ok();
-    })
-    .RequireAuthorization();
-
-app.MapGroup("v1/identity")
-    .WithTags("Identity")
-    .MapGet("/roles", (ClaimsPrincipal user) =>
-    {
-        if(user.Identity is null || !user.Identity.IsAuthenticated)
-            return Results.Unauthorized();
-        
-        var identity = (ClaimsIdentity)user.Identity;
-        var roles = identity.FindAll(identity.RoleClaimType)
-            .Select(c => new
-            {
-                c.Issuer,
-                c.OriginalIssuer,
-                c.Type,
-                c.Value,
-                c.ValueType
-            });
-
-        return TypedResults.Json(roles);
-    })
-    .RequireAuthorization();
-
 
 app.Run();
