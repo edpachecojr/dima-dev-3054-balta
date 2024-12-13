24b05d22483315af860ab97cfd3464888bc91b90
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2024-12-12 23:07:41 -0300

    feat: configura identity no startup da aplicacao

--- src/Dima.Api/Program.cs
+++ src/Dima.Api/Program.cs
@@ -1,7 +1,10 @@
+using System.Security.Claims;
 using Dima.Api.Data;
 using Dima.Api.Endpoints;
 using Dima.Api.Handlers;
+using Dima.Api.Models;
 using Dima.Core.Handlers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,16 +13,64 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 builder.Services.AddDbContext<AppDbContext>(x => { x.UseSqlServer(connectionString); });
 
+builder.Services.AddIdentityApiEndpoints<User>()
+    .AddRoles<IdentityRole<long>>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddApiEndpoints();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(x => { x.CustomSchemaIds(n => n.FullName); });
+
+
 builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
+
 app.MapGet("/", () => new { message = "Ok"});
 app.MapEndpoints();
+
+app.MapGroup("/v1/identity")
+    .WithTags("Identity")
+    .MapIdentityApi<User>();
+
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapPost("/logout", (SignInManager<User> signInManager) =>
+    {
+        signInManager.SignOutAsync();
+        return Results.Ok();
+    })
+    .RequireAuthorization();
+
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapGet("/roles", (ClaimsPrincipal user) =>
+    {
+        if(user.Identity is null || !user.Identity.IsAuthenticated)
+            return Results.Unauthorized();
+        
+        var identity = (ClaimsIdentity)user.Identity;
+        var roles = identity.FindAll(identity.RoleClaimType)
+            .Select(c => new
+            {
+                c.Issuer,
+                c.OriginalIssuer,
+                c.Type,
+                c.Value,
+                c.ValueType
+            });
+
+        return TypedResults.Json(roles);
+    })
+    .RequireAuthorization();
+
 
 app.Run();
