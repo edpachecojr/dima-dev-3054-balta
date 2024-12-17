1b82d6809d246f6195335a70994b7fb1c1c6b797
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2024-12-17 15:46:18 -0300

    feat: adiciona AccountHandler

---
+++ src/Dima.Web/Handlers/AccountHandler.cs
@@ -0,0 +1,35 @@
+using System.Net.Http.Json;
+using System.Text;
+using Dima.Core.Handlers;
+using Dima.Core.Requests.Account;
+using Dima.Core.Responses;
+
+namespace Dima.Web.Handlers;
+
+public class AccountHandler(IHttpClientFactory httpClientFactory) : IAccountHandler
+{
+    private readonly HttpClient _httpClient = httpClientFactory.CreateClient(Configuration.HttpClientName);
+    private readonly string _identityBasePath = "v1/identity";
+    
+    public async Task<Response<string>> LoginAsync(LoginRequest request)
+    {
+        var result = await _httpClient.PostAsJsonAsync($"{_identityBasePath}/login?useCookies=true", request);
+        return result.IsSuccessStatusCode 
+            ? new Response<string>("Login realizado com sucesso", 204, "") 
+            : new Response<string>(null, (int)result.StatusCode, "Não foi possível realizar login");
+    }
+
+    public async Task<Response<string>> RegisterAsync(RegisterRequest request)
+    {
+        var result = await _httpClient.PostAsJsonAsync($"{_identityBasePath}/register", request);
+        return result.IsSuccessStatusCode 
+            ? new Response<string>("Cadastro realizado com sucesso", 201, "") 
+            : new Response<string>(null, (int)result.StatusCode, "Não foi possível realizar seu cadastro");
+    }
+
+    public async Task LogoutAsync()
+    {
+        var emptyContent = new StringContent("{}", Encoding.UTF8, "application/json");
+        await _httpClient.PostAsJsonAsync($"{_identityBasePath}/logout", emptyContent);
+    }
+}
