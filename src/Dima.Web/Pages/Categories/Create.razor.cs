10bc5a8849b80ce4aa87fc6211514668a27cd1aa
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2024-12-20 10:35:31 -0300

    feat: implementa comportamento CreateCategory

---
+++ src/Dima.Web/Pages/Categories/Create.razor.cs
@@ -0,0 +1,48 @@
+using Dima.Core.Handlers;
+using Dima.Core.Requests.Categories;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Dima.Web.Pages.Categories;
+
+public partial class CreateCategoryPage : ComponentBase
+{
+    public bool IsBusy { get; set; } = false;
+    public CreateCategoryRequest InputModel { get; set; } = new();
+    
+    [Inject]
+    public ICategoryHandler Handler { get; set; } = null!;
+    
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = null!;
+    
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+
+    public async Task OnValidSubmitAsync()
+    {
+        IsBusy = true;
+
+        try
+        {
+            var result = await Handler.CreateAsync(InputModel);
+            if (result.IsSuccess)
+            {
+                Snackbar.Add($"{result.Message}", Severity.Success);
+                NavigationManager.NavigateTo("/categories");
+            }
+            else
+            {
+                Snackbar.Add($"{result.Message}", Severity.Error);
+            }
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add($"{ex.Message}", Severity.Error);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+}
